f2206ccdbe6eea92ac84ac8bd9fd30bf374f9f64
George_hu <huygeorge@163.com>
2019-11-13 01:18:39 +0800

    add pie

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -7,18 +7,68 @@ export default {
   name: 'piechart',
   data() {
     return {
-      dataArr:[]
+      dataArr: []
     };
   },
-  created(){
+  created() {
     this.dataArr = []
   },
-  mounted(){
+  mounted() {
     this.initPie();
   },
-  methods:{
-    initPie(){
-
+  methods: {
+    initPie() {
+      
+      option = {
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        legend: {
+          orient: '',
+          type: 'scroll',
+          bottom: 'center',
+          right: '10',
+          align: 'right',
+          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎', '搜索', '引擎', '擎']
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['30%', '35%'],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: '30',
+                  fontWeight: 'bold'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 234, name: '联盟广告' },
+              { value: 135, name: '视频广告' },
+              { value: 1548, name: '搜索引擎' },
+              { value: 1548, name: '搜索' },
+              { value: 1548, name: '引擎' },
+              { value: 1548, name: '擎' },
+            ]
+          }
+        ]
+      };
     }
   }
 };
